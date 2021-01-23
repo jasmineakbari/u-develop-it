759a4ab0373047a286423a35191593925dea1cc1
unknown <jasmineakbari@yahoo.com>
2021-01-22 22:11:12 -0500

    updated get routes for all candidates and single candidate and joined party affiliations

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ app.get('/', (req, res) => {
 
 // this will show the data from the candidates table in db file
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id`;
     const params = [];
 
     db.all(sql, params, (err, rows) => {
@@ -45,8 +49,12 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates
-                WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if(err) {
